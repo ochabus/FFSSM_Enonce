aeb663418c9b291e13882d7d36707721a06b6472
chabus <chabus@macbook-air-de-chabus.home>
2021-12-07 20:14:55 +0100

    restification + ajout des tests

--- src/test/java/PlongeeTest.java
+++ src/test/java/PlongeeTest.java
@@ -3,6 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import FFSSM.Club;
+import FFSSM.Embauche;
+import FFSSM.Licence;
+import FFSSM.Moniteur;
+import FFSSM.Plongee;
+import FFSSM.Plongeur;
+import java.time.LocalDate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,28 +23,22 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class PlongeeTest {
     
-    public PlongeeTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
+    Licence l1;
+    Moniteur Guillaume;// L'objet à tester
+    Plongeur plongeur;
+    Club dauphin;
+    Plongee plong1;
+    Embauche e1;
+
     @BeforeEach
+
     public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+        // Initialiser les objets utilisés dans les tests
+        dauphin = new Club(Guillaume, "Les Dauphins", "0729017354");
+        plongeur = new Plongeur("86", "ALBAN", "Thibault", "28 pas. hermitte", "0678452543", LocalDate.of(1990, 12, 8));
+        Guillaume = new Moniteur(Guillaume, dauphin, 4, "29", "CASTILLO", "Guillaume", "avenue de toulouse", "0637937900", LocalDate.of(1977, 4, 11));
+        l1 = new Licence(Guillaume, "08917", LocalDate.of(2020, 9, 26), dauphin);
+        e1 = new Embauche(LocalDate.of(2021, 12, 7), Guillaume, dauphin);
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
